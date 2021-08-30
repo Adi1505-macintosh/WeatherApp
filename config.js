7d8a909a4582d9255723fcff5d79cbac70ef5298
Adi1505-macintosh <adityadarekar1505@gmail.com>
2021-08-30 22:13:27 +0530

    First Commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "82d4ed1afc547bd4d52b42b357843d48"
+    }
+}
+
+module.exports = constants;
